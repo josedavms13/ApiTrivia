710b8cd4f43dcb7b9b4226c17c690bf2dcc3bb9f
Brandon Leyva <bgleyva3@gmail.com>
2021-04-25 20:44:55 -0500

    "Added DisplayQAs"

--- 00ignore/brandon_pruebas.mjs
+++ 00ignore/brandon_pruebas.mjs
@@ -1,4 +1,4 @@
-
+/* 
 localStorage.setItem("wuwu", 11);
 let storaged = localStorage.getItem("wuwu");
 console.log(storaged)
@@ -9,4 +9,12 @@ console.log(storaged)
 
 if(storaged === null){
     console.log("nullo")
-}
+} */
+
+const randomNotRounded = Math.random()*(1-0);
+const randomNumber = Math.round(randomNotRounded)
+console.log(randomNumber)
+
+const randomNotRounded2 = Math.random()*(3-0);
+const randomNumber2 = Math.round(randomNotRounded2)
+console.log(randomNumber2)
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import RoundSystem from "./Classes/RoundSystem.js";
 import RoundSystemMode2 from "./Classes/RoundSystemGameMode2.js"
 import AnswerCheck from "./Classes/AnswerCheck.js";
 import UI from "./Classes/UI.js";
+import DisplayQAs from "./Classes/DisplayQAs.js"
+import PruebaObject from "./Classes/PruebaObject.js"
 
 
 //#region GLOBALS
@@ -18,17 +20,16 @@ let CHECKER = null
 //---------------------------------------------------------------
 
 // const Questions = new RoundSystem(3);
-localStorage.setItem("level", 2)
+ 
 
 function mode1Handler(){
-    const storageLevel = localStorage.getItem("level");
+    const storageLevelJSON = localStorage.getItem("level");
+    const storageLevel = JSON.parse(storageLevelJSON);
     if(storageLevel === null ){
-        /* console.log("No había level guardado")
-        console.log("--------------------------------") */
+        //console.log(`No hay level en storage`)
         ModeChanger(1,1);
     }else{
-        /* console.log(`El level guardado es: ${storageLevel}`)
-        console.log("--------------------------------") */
+        //console.log(`level en storage: ${storageLevel}`)
         ModeChanger(1, storageLevel);
     }
 }
@@ -40,7 +41,9 @@ function mode1Handler(){
 function ModeChanger(mode, parameters) {
     let Questions = null
     if(mode === 1){
+        //console.log(parameters)
         Questions = new RoundSystem(parameters);
+        //console.log("mode1")
 
     }
     if(mode === 2){
@@ -63,17 +66,17 @@ function ModeChanger(mode, parameters) {
 
             ///---- > Here is hapens AFTER! the object is created.
 
-            console.log(QUESTIONS_OBJECT.Questions);
-
-            // CHECKER = new AnswerCheck(testAnswers, toTestObject.CorrectAnswers);
-
-
-
-
+            console.log(QUESTIONS_OBJECT);
+            DisplayQAs.run(QUESTIONS_OBJECT);
+            //PruebaObject.run(QUESTIONS_OBJECT);
+        
         }, 1000)
 
     }, 3000)
 }
+
+
+
 
 //---------------------------------------------------------------
 
@@ -88,6 +91,87 @@ function ModeChanger(mode, parameters) {
 
 
 //#region  TO TEST AND CONNECT
+
+/* function displayQuestionsAnswers(){
+
+    const randomPosition = answersOrder();
+    const allAnswersArray = concatAllAnswers(randomPosition);
+
+    console.log(randomPosition)
+    console.log(all)
+    //----------------------------------------------------
+    
+    //Ques & Ans 1
+    const Q1 = document.getElementById("Q1");
+    Q1.innerHTML = QUESTIONS_OBJECT.Questions[0];
+    const Answers1 = document.getElementById("Answers1");
+    printAnswers(allAnswersArray[0], "Answers1");
+    console.log("Aquí llega-1")
+
+    //Ques & Ans 2
+    const Q2 = document.getElementById("Q2");
+    Q2.innerHTML = QUESTIONS_OBJECT.Questions[1];
+    const Answers2 = document.getElementById("Answers2");
+    printAnswers(allAnswersArray[1], "Answers2");
+    console.log("Aquí llega-2")
+
+    //Ques & Ans 3
+    const Q3 = document.getElementById("Q3");
+    Q3.innerHTML = QUESTIONS_OBJECT.Questions[2];
+    const Answers3 = document.getElementById("Answers3");
+    printAnswers(allAnswersArray[2], "Answers3");
+    console.log("Aquí llega-3")
+    
+}
+
+
+
+//#region Array completo de repuestas en orden aleatorio 
+function answersOrder(){
+    const incorrectAnswersArray = QUESTIONS_OBJECT.IncorrectAnswers;
+    //console.log(incorrectAnswersArray)
+    const randomPosition = [];
+    for(let i=0; i < 3; i++){
+        if(incorrectAnswersArray[i].length === 3){
+            const randomNotRounded = Math.random()*(3-0);
+            const randomNumber = Math.round(randomNotRounded)
+            //console.log(randomNumber)
+            randomPosition.push(randomNumber);
+
+        }else{
+            randomPosition.push(null);
+        }
+    }
+    return(randomPosition)
+    //console.log(randomPosition);
+}
+
+function concatAllAnswers(randomPosition){
+    const correctAnswersArray = QUESTIONS_OBJECT.CorrectAnswers;
+    const incorrectAnswersArray = QUESTIONS_OBJECT.IncorrectAnswers;
+    const allAnswersArray = incorrectAnswersArray;
+    for(let i=0; i < incorrectAnswersArray.length; i++){
+        if(randomPosition[i] !== null){
+            const randomIndex = randomPosition[i];
+            const correctAnswer = correctAnswersArray[i];
+            allAnswersArray[i].splice(randomIndex, 0, correctAnswer[i]);
+        } else {
+            allAnswersArray[i] = ["True", "False"];
+        }
+    }
+    return allAnswersArray;
+}
+
+function printAnswers(answers, idName){
+    const divAnswers = document.getElementById(idName);
+    for(let i=0; i < answers.length; i++){
+        divAnswers.innerHTML += `<button>${answers[i]}</button>`
+    }
+}
+
+//#endregion  */
+
+
 
 //#region  Checking System
 const toTestObject = { 
@@ -113,7 +197,7 @@ const testAnswers = [toTestObject.IncorrectAnswers[1][0], toTestObject.CorrectAn
 //#region  ModeSwitcher
 
 // ModeChanger(mode, lvl);
-ModeChanger(1, 1);
+//ModeChanger(1, 2);
 
 
 // MODE 2
